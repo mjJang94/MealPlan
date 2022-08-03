d81bfb79b8c728f5a09e815aac15e183a044b99d
mj <mj.jang@kiwiplus.io>
2022-08-03 10:11:14 +0900

    누락 파일 추가

---
+++ buildSrc/src/main/java/Dependency.kt
@@ -0,0 +1,34 @@
+
+object VERSION{
+    const val COMPILE_SDK = 32
+    const val TARGET_SDK = 32
+    const val BUILD_TOOLS = "32.0.0"
+
+    const val GRADLE = "7.1.2"
+    const val KOTLIN = "1.6.10"
+
+    const val GOOGLE_GMS = "4.3.13"
+
+    const val CORE = "1.8.0"
+    const val APP_COMPAT = "1.4.2"
+    const val CONSTRAINT_LAYOUT = "2.1.4"
+    const val ACTIVITY = "1.5.0"
+    const val FRAGMENT = "1.5.0"
+    const val LIFECYCLE = "2.5.0"
+    const val RECYCLERVIEW = "1.2.1"
+    const val PAGING = "3.1.1"
+    const val MATERIAL = "1.6.1"
+
+    const val RETROFIT = "2.9.0"
+    const val RETROFIT_LOGGING = "4.9.3"
+
+    const val COROUTINE = "1.6.2"
+
+    const val HILT = "2.41"
+
+    const val GLIDE = "4.13.2"
+
+    const val FIREBASE_REAL_TIME_DATABASE = "30.1.0"
+
+    const val TIMBER = "4.7.1"
+}
